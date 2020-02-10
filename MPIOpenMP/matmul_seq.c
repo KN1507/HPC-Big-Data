cc0a317b5df61619a23cb0070bb196b1f3cfce1a
Neeraj-b025 <neeraj.sathian@gmail.com>
2020-02-10 19:11:04 +0100

    Added timer in seq

--- MPIOpenMP/matmul_seq.c
+++ MPIOpenMP/matmul_seq.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define SIZE 3  // Size of matrix and vector
+#define SIZE 8  // Size of matrix and vector
+
+#define R 10
 
 int matrix[SIZE][SIZE], vector[SIZE], result[SIZE];
 
@@ -9,6 +12,7 @@ int main(int argc, char *argv[])
 {
   int rank, size, matrix_start, matrix_end, n;
   n = SIZE;
+  clock_t tic = clock();
   	printf("Fill Matrix : \n");
             for (int i=0; i<n*n; i++) {
                 for (int j=0;j<n;++j)
@@ -22,11 +26,15 @@ int main(int argc, char *argv[])
             }
 
   int j=0; //Row no..
-  for (int i=0; i<n; i++) {
-      for (int j=0;j<n;++j) {
-              result[i] += matrix[i][j] * vector[j];
+  for (int p=0; p<R; ++p) {
+      for (int i=0; i<n; i++) {
+        for (int j=0;j<n;++j) {
+            result[i] += matrix[i][j] * vector[j];
+        }
       }
   }
+
+  clock_t toc = clock();
 
     printf("Matrix: \n");
     for(int i=0;i<n;++i) {
@@ -35,16 +43,19 @@ int main(int argc, char *argv[])
         }
         printf("\n");
     }
-
+    printf("\n");
     printf("\nVector: \n");
     for(int i=0;i<n;++i){
         printf("%d\t",vector[i]);
     }
-
+    printf("\n");
     printf("\nResult: \n");
     for(int i=0;i<n;++i){
         printf("%d\t",result[i]);
     }
+    printf("\n");
+    printf("Elapsed time: %f seconds\n",(double)(toc - tic) / CLOCKS_PER_SEC);
 
   return 0;
 }
+
